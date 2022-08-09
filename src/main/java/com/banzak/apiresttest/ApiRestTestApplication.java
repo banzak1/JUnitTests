e0a3e4e879a8bffcc8231546285d75d09e7b24e3
banzak1 <leosantanabr@gmail.com>
2022-08-09 17:16:16 -0300

    Criando model User

--- src/main/java/com/banzak/apiresttest/ApiRestTestApplication.java
+++ src/main/java/com/banzak/apiresttest/ApiRestTestApplication.java
@@ -1,5 +1,6 @@
 package com.banzak.apiresttest;
 
+import com.banzak.apiresttest.model.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/banzak/apiresttest/model/User.java
@@ -0,0 +1,21 @@
+package com.banzak.apiresttest.model;
+
+import lombok.*;
+import javax.persistence.*;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
